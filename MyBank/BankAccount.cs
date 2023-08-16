f527c4149712db99d89ffed2ebb92fdb5343773c
Molly Gallagher Wickham <mollyg210@gmail.com>
2023-08-16 16:36:12 +0100

    MyBank App

---
+++ MyBank/BankAccount.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Transactions;
+
+namespace MyBank
+{
+    public class BankAccount
+    {
+
+        public string Number { get; }
+
+        public string Owner { get; set; }
+
+        public bool ShowMenu { get; set; }
+
+        public decimal Balance {
+            get
+            {
+                // sets original balance to 0
+                decimal balance = 0;
+                // goes through all transactions
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            // first transaction is the initial balance
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        // should an 'out' be
+        public void Menu()
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1. Check Balance");
+            Console.WriteLine("2. Make a Deposit");
+            Console.WriteLine("3. Make a Withdrawl");
+            Console.WriteLine("4. Get Account History");
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            // if you try to deposit negative amount
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+
+            // if the balance minus the amount to withdraw would result in less than 0
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            // report is a StringBuilder type
+            var report = new StringBuilder();
+
+            // Creates Header
+            report.AppendLine("Date\t\tAmount\tNote");
+            foreach (var item in allTransactions)
+            {
+                // Creates Row
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+            }
+            // As report is of StringBuilder Type, this needs to convert to String
+            return report.ToString();
+        }
+
+        public void EndOfService()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write(".");
+            }
+        }
+    }
+}
+
---
+++ MyBank/bin/Debug/net7.0/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creates new bank account with £1000 inside
+            Console.WriteLine("Would you like to create a bank account? Y/N");
+            var userCreate = Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+
+
+            var account = new BankAccount(userName, 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            Console.WriteLine($"Welcome, {account.Owner}.");
+
+            account.Menu();
+            var menuResponse = Console.ReadLine();
+            // selects to view account balance
+
+            if (menuResponse == "1")
+            {
+                Console.WriteLine($"Your account balance is: {account.Balance}");
+            }
+            // Selects to make a deposit
+            else if (menuResponse == "2")
+            {
+                Console.WriteLine($"How much would you like to deposit?");
+                string depositAmountInput = Console.ReadLine();
+                int depositAmount = Int32.Parse(depositAmountInput);
+                Console.WriteLine($"Please create a reference");
+                string depositNote = Console.ReadLine();
+
+                account.MakeDeposit(depositAmount, DateTime.Now, depositNote);
+                Console.WriteLine($"Your account balance is: {account.Balance}");
+            }
+            // selects to make withdrawal
+            else if (menuResponse == "3")
+            {
+                Console.WriteLine($"How much would you like to withdraw?");
+                string withdrawAmountInput = Console.ReadLine();
+                int withdrawAmount = Int32.Parse(withdrawAmountInput);
+                Console.WriteLine($"Please create a reference");
+                string withdrawNote = Console.ReadLine();
+
+                account.MakeWithdrawal(withdrawAmount, DateTime.Now, withdrawNote);
+                Console.WriteLine($"Your account balance is: {account.Balance}");
+            }
+            // selects to view account history
+            else if (menuResponse == "4")
+            {
+                Console.WriteLine("Here is your account history");
+                Console.WriteLine(account.GetAccountHistory());
+            }
+            else if (menuResponse == "5")
+            {
+
+            }
+
+
+            account.EndOfService();
+            Console.WriteLine(".");
+            account.Menu();
+
+            // write logic to get the if statement to run again
+
+            // Test that you can't overdraw
+            //try
+            //{
+            //    account.MakeWithdrawal(70000, DateTime.Now, "Tiny handbag");
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    //Console.WriteLine(e.ToString());
+            //}
+
+            //// Test that the initial balances must be positive
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    //Console.WriteLine(e.ToString());
+            //}
+
+            //account.MakeWithdrawal(50, DateTime.Now, "Computer Game");
+            //Console.WriteLine(account.Balance);
+
+            //Console.WriteLine(account.GetAccountHistory());
+
+
+        }
+    }
+}
